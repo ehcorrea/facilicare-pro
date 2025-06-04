45ab8bdbe738bbc24ed4f1eba0a733fd11e63507
Emilio Correa <ehcorrea60@gmail.com>
2025-06-04 15:46:56 -0300

    chore: add types

--- src/@types/emotion.d.ts
+++ src/@types/emotion.d.ts
@@ -1,11 +1,12 @@
-// src/@types/emotion.d.ts
 import '@emotion/react';
 import { ThemeType } from '@/constants/theme';
 
 declare module '@emotion/react' {
   export type Theme = ThemeType;
+  export function useTheme(): ThemeType;
 }
 
 declare module '@emotion/native' {
   export function css(...args: unknown[]): string;
+  export type Theme = ThemeType;
 }
