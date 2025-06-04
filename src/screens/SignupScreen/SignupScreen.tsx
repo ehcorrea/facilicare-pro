99c8f917fdaae6a2f31296d175b6a92b71bf479d
Emilio Correa <ehcorrea60@gmail.com>
2025-06-04 20:52:52 -0300

    refactor: update use of Spacing component

--- src/screens/SignupScreen/SignupScreen.tsx
+++ src/screens/SignupScreen/SignupScreen.tsx
@@ -20,33 +20,36 @@ export function SignupScreen() {
         showsVerticalScrollIndicator={false}
       >
         <Text.Title size="xhuge">Criar Conta</Text.Title>
-        <Spancing y={15} />
+        <Spancing y="15" />
         <Input label="Nome" placeholder="Nome Completo" />
+        <Spancing y="5" />
         <Input label="Email" placeholder="exemplo@email.com" />
+        <Spancing y="5" />
         <Input label="Senha" variant="password" placeholder="••••••" />
+        <Spancing y="5" />
         <Input label="Repetir senha" variant="password" placeholder="•••••••" />
-        <Spancing y={5} />
+        <Spancing y="15" />
         <Button elevation className="w-[60%] self-center">
           CRIAR CONTA
         </Button>
-        <Spancing y={15} />
+        <Spancing y="5" />
         <View className="flex-row self-center">
           <S.Label>Já tem um conta?</S.Label>
-          <Spancing x={3} />
+          <Spancing x="3" />
           <Text palette="primary">Entrar</Text>
         </View>
-        <Spancing y={20} />
+        <Spancing y="15" />
         <View className="flex-row flex-1 items-center">
           <Line />
-          <Spancing x={13} />
-          <S.Label>Criar com</S.Label>
-          <Spancing x={13} />
+          <Spancing x="10" />
+          <S.Label size="large">criar com</S.Label>
+          <Spancing x="10" />
           <Line />
         </View>
-        <Spancing y={10} />
+        <Spancing y="15" />
         <View className="flex-row">
           <Button.Account variant="facebook" />
-          <Spancing x={10} />
+          <Spancing x="10" />
           <Button.Account variant="google" />
         </View>
       </ScrollView>
